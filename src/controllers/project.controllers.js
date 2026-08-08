15530bdb2141a1a51a523c1134aa1336958a87b0
krishna <krishna.ch1406@gmail.com>
2026-08-08 19:34:41 +0530

    defining the createProject controller

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -5,7 +5,8 @@ import {ProjectMember} from "../models/projectmember.model.js"
 import {ApiResponse} from "../utils/apiresponse.js"
 import {ApiError} from "../utils/api-error.js"
 import {asyncHandler} from "../utils/async-handler.js"
-
+import mongoose from "mongoose"
+import { UserRolesEnum } from "../utils/constants.js"
 
 const getProjects = asyncHandler(async(req,res)=>{
     //test
@@ -16,7 +17,27 @@ const getProjectById = asyncHandler(async(req,res)=>{
 })
 
 const createProject = asyncHandler(async(req,res)=>{
-    //test
+    const {name,description}= req.body
+
+    await Project.create({
+        name,
+        description,
+        createdBy: new mongoose.Types.ObjectId(req.user._id),
+    })
+
+    await ProjectMember.create({
+        user: new mongoose.Types.ObjectId(req.user._id),
+        project: new mongoose.Types.ObjectId(project._id),
+        role: UserRolesEnum.ADMIN
+    })
+
+    return res
+    .status(201)
+    .json(new ApiResponse(
+        201,
+        project,
+        "Project created successfully"
+    ))
 })
 
 const updateProject = asyncHandler(async(req,res)=>{
