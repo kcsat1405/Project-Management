ce86450dc96e5108acff1c1bbcad4e087d0e3426
krishna <krishna.ch1406@gmail.com>
2026-08-08 20:13:00 +0530

    updating the deleteProject controller

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -67,7 +67,14 @@ const updateProject = asyncHandler(async(req,res)=>{
 })
 
 const deleteProject = asyncHandler(async(req,res)=>{
-    //test
+    const{projectId}=req.params
+    const project = await Project.findByIdAndDelete(projectId)
+    if(!project){
+        throw new ApiError(404,"Project not found")
+    }
+    return res
+    .status(200)
+    .json(new ApiResponse(200, project, "Project deleted successfully"))
 })
 
 const addMembersToProject = asyncHandler(async(req,res)=>{
