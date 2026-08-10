90a021d1fb0e12a3f5e85aa204692746396ee9e3
krishna <krishna.ch1406@gmail.com>
2026-08-10 14:12:16 +0530

    updated by updateMembers controller by removing extra database operations

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -223,31 +223,27 @@ const getProjectMembers = asyncHandler(async(req,res)=>{
 const updateMemberRole = asyncHandler(async(req,res)=>{
     const {projectId, userId}= req.params
     const {newRole}=req.body
+
     if(!AvailableUserRole.includes(newRole)){
         throw new ApiError(400, "Invalid role")
     }
-    let projectMember = await ProjectMember.findOne({
+    let projectMember = await ProjectMember.findOneAndUpdate(
+        {
         project: new mongoose.Types.ObjectId(projectId),
         user: new mongoose.Types.ObjectId(userId)
-    })
-
-    if(!projectMember){
-        throw new ApiError(404, "Project member not found")
-    }
-
-    projectMember =await ProjectMember.findByIdAndUpdate(
-        projectMember._id,
+        },
         {
             role: newRole
         },
         {
-            new: true,
+            new:true
         }
-    )
-     if(!projectMember){  //if updation is done then only we will receive this projectMember
+)
+
+    if(!projectMember){
         throw new ApiError(404, "Project member not found")
     }
-
+    
     return res
     .status(200)
     .json(new ApiResponse(200, projectMember, "Project member role updated successfully"))
