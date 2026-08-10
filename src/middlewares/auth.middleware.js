74f0478cf085334052d5172b789c8c0e948de5ca
krishna <krishna.ch1406@gmail.com>
2026-08-10 14:37:52 +0530

    in validate extracted the project

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,5 @@
 import {User} from "../models/user.models.js"
+import {ProjectMember} from "../models/projectmember.model.js"
 import {asyncHandler} from "../utils/async-handler.js"
 import {ApiError} from "../utils/api-error.js"
 import jwt from "jsonwebtoken"
@@ -24,3 +25,23 @@ export const verifyJWT= asyncHandler(async(req,res,next)=>{
     }
 })
 
+export const validateProjectPermission= (roles=[])=>{
+    asyncHandler(async(req,res,next)=>{
+        const {projectId}= req.params
+
+        if(!projectId){
+            throw new ApiError(400, "project id is missing")
+        }
+
+        const project= await ProjectMember.findOne({
+            user: new mongoose.Types.ObjectId(req.user._id),
+            project: new mongoose.Types.ObjectId(projectId),
+        })
+
+        if(!project){
+            throw new ApiError(400, "Project not found")
+        }
+
+        const givenRole= project?.role
+    })
+}
